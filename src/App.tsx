808c4c5e2ab707df2ea435781d7e4ee90ac49f98
Blame <17600160566@163.com>
2025-12-16 14:21:28 +0800

    加入了ts，tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">
+        React + TS + Vite + Tailwind 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),tailwindcss()],
   // ⚠️ 关键配置：这里的 'blame-web-note' 是你你实际的 GitHub 仓库名
   base: '/blame-web-note/', 
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 })
